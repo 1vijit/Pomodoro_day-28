ba0cbdb28fc486c08f63e3ddffe43574fab5b6bd
1vijit <111475826+1vijit@users.noreply.github.com>
2023-02-10 15:54:53 -0500

    final push

--- main.py
+++ main.py
@@ -6,38 +6,61 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 0.1#25
+SHORT_BREAK_MIN = 0.05#5
+LONG_BREAK_MIN = 0.05#20
 
+timer = None
 reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    title_label.config(text=" ")
+    check_marks.config(text=" ")
+    canvas.itemconfig(timer_text, text=f"00:00")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
     global reps
+    reps += 1
     work_sec = WORK_MIN*60
     short_break_sec = SHORT_BREAK_MIN*60
     long_break_sec = LONG_BREAK_MIN*60
     count_down(5*60)
 
+    if reps%8 ==0:
+        count_down(long_break_sec)
+        title_label.config(text="Break")
+    elif reps%2 !=0:
+        count_down(short_break_sec)
+        title_label.config(text="Break")
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work")
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     count_min = math.floor(count/60)
     count_sec = count%60
-
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec:02}")
     if count>0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        check_marks.config(text=marks)
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-
-
-# window.after(1000, count_down, count-1)
 
 title_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
 title_label.grid(column=1, row=0)
@@ -50,7 +73,7 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-start_button = Button(text="Reset", highlightthickness=0)
+start_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 start_button.grid(column=2, row=2)
 
 check_marks = Label(text="✓", fg=GREEN, bg=YELLOW)
